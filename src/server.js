a2ce64a35edb9b06b14cd53d60d3b8b3d0d8d343
Mostafa Hisham <mmham3919@gmail.com>
2021-05-05 00:30:21 +0200

    adding swagger conf in server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./common/swagger');
 
 const app = express();
 
@@ -9,5 +11,12 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+// Swagger URL
+app.use(
+    '/api/v0/explore',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocs)
+ );
 
 module.exports = app;
